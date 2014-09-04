6e1eed12223e7fda902238b8c216919227cc4e27
anestv <anestv@users.noreply.github.com>
2014-09-04 12:57:52 +0300

    Improved pending.php
    Sort unanswered question from oldest to newest and show only the 50
    first
    If no questions are returned, show div#noQs
    Timeasked formatted to look nicer

--- pending.php
+++ pending.php
@@ -3,10 +3,11 @@
 if(empty($user))
   terminate('You are not logged in <a href="login.php">Log in</a>', 401);
 
-$query = "SELECT * FROM questions WHERE touser = '$user' AND answer IS NULL ;";
+$query = "SELECT * FROM questions WHERE touser = '$user'
+  AND answer IS NULL ORDER BY timeasked LIMIT 50;";
 $questions_raw = $con->query($query);
 
-if(!$questions_raw) terminate('A server error has occurred', 500);
+if (!$questions_raw) terminate('A server error has occurred '.$con->error, 500);
 ?>
 
 <html>
@@ -21,10 +22,15 @@ Hello, <?=$user?>
 <div id="qContainer">
 
 <?php
-while($row = mysqli_fetch_array($questions_raw)) {
-  echo '<div class="question"><div class="links"><a href="answer.php?qid='. $row['id']. '">Answer</a>';
-  echo '<br><a class="red" href="delquest.php?qid='. $row['id']. '">Delete</a></div>';
-  echo '<span class="date">Asked: '. $row['timeasked'] .'</span><h3>'. $row['question'] .'</h3></div>';
+
+if ($questions_raw->num_rows === 0)
+  echo '<div id="noQs">There are no more questions for you to answer!</div>';
+else while ($row = $questions_raw->fetch_array()) {
+  $timeasked = date('G:i \o\n l j/n/y', strtotime($row['timeasked']));
+  echo '<div class="question"><div class="links"><a href="answer.php?qid=';
+  echo $row['id']. '">Answer</a>'. '<br><a class="red" href="deleteq.php?qid=';
+  echo $row['id']. '">Delete</a></div>'. '<a class="date">Asked: <time>';
+  echo "$timeasked</time></a><h3>" . $row['question'] .'</h3></div>';
 }
 ?>
 
