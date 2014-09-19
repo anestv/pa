288bf6a700a29d69739be03a3efae3142e5cb005
anestv <anestv@users.noreply.github.com>
2014-09-19 19:37:43 +0300

    SUI to deleteacc and pending.php

--- deleteacc.php
+++ deleteacc.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Delete your account - PrivateAsk</title>
+  <link rel="stylesheet" type="text/css" href="css/general.css">
+  <link rel="stylesheet" type="text/css" href="css/semantic.min.css">
+</head>
+<body>
 <?php
 
 if (empty($user))
@@ -10,44 +18,67 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){ //thelei na to diagrapsei
   if ($_POST["user"] !== $user)
     terminate("You did not enter your account's username", 400);
   
-  //TODO password get, hash+Salt, save in var
-  
   if (isset($_POST['pass']) and trim($_POST['pass']))
   	$pass = $_POST['pass'];
   else terminate('You did not enter a password', 400);
   
-  $userobj = mysqli_fetch_array($con->query("SELECT username , hs_pass FROM users WHERE username = '$user';"));
+  $userobj = $con->query("SELECT hs_pass FROM users WHERE username = '$user';")->fetch_array();
   
   if (!password_verify($pass, $userobj['hs_pass']))
   	terminate('The password you entered is incorrect');
   
 
-  $query = "UPDATE users SET deleteon = CURRENT_DATE + INTERVAL 7 DAY WHERE username = '$user';"; //TODO where password=...
+  $query = "UPDATE users SET deleteon = CURRENT_DATE + INTERVAL 7 DAY WHERE username = '$user';";
   $res = $con->query($query);
   
-  if (!$res) terminate('A server error occurred', 500);
+  if (!$res) terminate('A server error occurred '. $con->error, 500);
   else {
-    echo "Your account will e deleted in 7 days. You have been logged out";
     session_destroy();
+    echo '<div class="ui success message"><i class="trash icon"></i>';
+	echo 'Your account will be deleted in 7 days. You have been logged out.';
+	echo ' You will be redirected to the main page</div>';
+    echo '<meta http-equiv="refresh" content="5; url=.">';
   }
 }
-
 ?>
+<main style="max-width:950px; margin:auto"><!-- could be id="qContainer" -->
 
+<h1 class="ui red inverted block top attached header">
+  <i class="trash icon"></i> Delete your account
+</h1>
+<div class="ui attached segment">
+  <p class="ui vertical segment">You are about to delete your account. Your profile and
+   questions you have answered will be erased. Questions you have asked anonymously will
+   be kept as they are, but questions you have asked publicly (i.e. your name appears in
+   the question) will no longer show your name.</p>
+  <p class="ui vertical segment">For a 7-day period the information mentioned above will
+   be kept hidden, and will be recoverable by logging in your account. After that period,
+   the changes described above will take place and they will be IRREVOCABLE and PERMANENT.
+   We will not be able to recover any data after that period.</p>
+  <p class="ui vertical segment">If you want to procceed, confirm the account's deletion
+   by typing your username and password.</p>
+</div>
 
-<h1>Delete your account</h1>
-<p>You are about to delete your account. Your profile and questions you have answered will be erased. 
-Questions you have asked anonymously will be kept as they are, but questions you have asked publicly 
-(i.e. your name appears in the question) will no longer show your name.</p>
-<p>For a 7-day period the information mentioned above will be kept hidden, and will be recoverable by 
-logging in your account. After that period, the changes described above will take place and they will 
-be IRREVOCABLE and PERMANENT. We will not be able to recover any data after that period.</p>
-<p>If you want to procceed, confirm the account's deletion by typing your username and password. </p>
-
-<form method="post">
-<input required name="user" type="text" placeholder="Username">
-<br>
-<input required name="pass" type="password" placeholder="Password">
-<br>
-<input type="submit" value="Delete">
+<form method="post" class="ui bottom attached form segment">
+  <div class="ui two fields">
+    <div class="field"><div class="ui left labeled icon input">
+      <i class="user icon"></i>
+      <input type="text" placeholder="Username" name="user" required>
+      <div class="ui corner label">
+        <i class="red asterisk icon"></i>
+      </div>
+    </div></div>
+    <div class="field"><div class="ui left labeled icon input">
+      <i class="lock icon"></i>
+      <input type="password" placeholder="Password" name="pass" required>
+      <div class="ui corner label">
+        <i class="red asterisk icon"></i>
+      </div>
+    </div></div>
+  </div>
+  <button type="submit" class="ui negative button">Delete</button>
 </form>
+
+</main>
+</body>
+</html>
