fdb3e856b6d5aaafa1fdf8b95cf26ed08057547e
anestv <anestv@users.noreply.github.com>
2014-10-03 15:25:32 +0300

    Profile bg and header colors changeable
    The text color of the header is the body's bg color
    The text font is changeable again.
    At db old 'textcolor' is now 'headcolor'

--- css/profileAppearance.dcss.php
+++ css/profileAppearance.dcss.php
@@ -12,17 +12,20 @@ if (empty($_GET['user']))
   termin('You did not specify a user', 400);
 
 $ownerName = $con->real_escape_string($_GET['user']);
-$query = "SELECT textcolor, textfont, backcolor FROM users WHERE username = '$ownerName';";
+$query = "SELECT headcolor, textfont, backcolor FROM users WHERE username = '$ownerName';";
 $owner = $con->query($query)->fetch_array();
 if ($owner === null)
   termin('This user does not exist or has deleted their account', 404);
 ?>
 
-* {
-  color: <?=$owner['textcolor']?>;
-  font-family: '<?=$owner['textfont']?>', Calibri, Arial, sans-serif;
+header#profileHeader {
+  background-color: <?=$owner['headcolor']?>!important;
+  color: <?=$owner['backcolor']?>!important;
 }
 
-header, div.question, button, #noQs, .warn, #askControls {
-  background-color: <?=$owner['backcolor']?>!important;
+body[data-owner] {
+  font-family: '<?=$owner['textfont']?>', Calibri, Arial, sans-serif;
+  background-color: <?=$owner['backcolor']?>;
+  background-image: linear-gradient(to bottom,
+    rgba(0, 0, 0, 0), rgba(60, 60, 60, 0.2));
 }
--- profile.php
+++ profile.php
@@ -22,9 +22,9 @@ if ($owner['deleteon'] !== null)
 <base href="/pa/">
 <title><?=$ownerName?> - PrivateAsk</title>
 <link rel="stylesheet" type="text/css" href="css/general.css">
-<!-- <link rel="stylesheet" type="text/css" href="res/fonts/customFonts.css">
-<link rel="stylesheet" type="text/css" href="css/profileAppearance.dcss.php?user=<?=$ownerName?>"> -->
+<link rel="stylesheet" type="text/css" href="res/fonts/customFonts.css">
 <link rel="stylesheet" type="text/css" href="css/semantic.min.css">
+<link rel="stylesheet" type="text/css" href="css/profileAppearance.dcss.php?user=<?=$ownerName?>">
 <meta charset="UTF-8"><!-- an exei elhnika -->
 
 <meta property="og:type" content="profile">
@@ -41,7 +41,7 @@ if ($owner['deleteon'] !== null)
 $see = $owner['whosees'];
 $ask = $owner['whoasks'];
 
-echo '<header><i class="user big circular icon"></i><h1>';
+echo '<header id="profileHeader"><i class="user big circular icon"></i><h1>';
 echo $owner['realname']. "</h1><span>Username: $ownerName</span></header>";
 
 
@@ -53,7 +53,7 @@ else if ($ask === 'friends' and !in_array($user, $ownerFr))
     '</i>Sorry, you do not have the right to ask a question</div>';
 else {
 ?>
-<form method="post" class="ask ui dimmable form" action="sent.php">
+<form method="post" class="ask ui dimmable form segment" action="sent.php">
 
   <div class="ui dimmer"><div class="content"><div class="center">
     Your question has been submitted!<br>Ask another one?
@@ -67,15 +67,21 @@ else {
   else if ($user)
     echo '<label for="publicaskcheckbox"><input type="checkbox" name="pubAsk"
     id="publicaskcheckbox">Show that I asked this question</label>';
-  
-  echo '<button type="submit">Ask</button></div></form>';
-}
+?>
+  <button type="submit" class="ui blue small icon animated button">
+    <div class="hidden content"><i class="question icon"></i></div>
+    <div class="visible content">Ask</div>
+  </button>
+  </div>
+</form>
+<?php }
 
 echo '<div id="qContainer">';
 include("loadquestions.php");
 if (http_response_code() === 204){
   $isLast = true;
-  echo '<div id="noQs">There are no questions to show</div>';
+  echo '<div class="center480 ui info message"><i class="info icon"></i>';
+  echo 'There are no questions to show</div>';
 }
 http_response_code(200);
 echo '</div>';
