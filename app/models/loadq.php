60dcfb1fd1f60b6a6e0b3024372ece3dbd8e1d72
anestv <anestv@users.noreply.github.com>
2014-12-19 23:24:08 +0200

    Rewrote m\LoadQ::main()
    Removed useless try{}
    Make use of m\QuestionSet
    Do not writeOut(), leave it for the views/controllers

--- app/models/loadq.php
+++ app/models/loadq.php
@@ -1,47 +1,31 @@
 <?php namespace models;
-use \Exception, \RuntimeException;
+use \Exception;
 
 class LoadQ extends \core\model {
   
   public static function main($owner, $offset = 0){
     if (!is_numeric($offset) or $offset < 0)
-      throw new InvalidArgumentException('The offset is not of correct type.');
+      throw new \InvalidArgumentException('The offset is not of correct type.');
     $offset = intval($offset);
     
-    try {
-      if (!($owner instanceof User))
-        $owner = new User($owner);
-      
-      if ($owner->deactivated)
-        throw new Exception('This user has deactivated their account.');
-      
-      if (! $owner->profileVisibleBy($GLOBALS['user']))
-        throw new Exception("You are not allowed to view this user's questions", 403);
-      
-      $query = "SELECT id FROM questions WHERE answer IS NOT NULL AND touser".
-        " = '$owner->username' ORDER BY timeanswered DESC LIMIT 11 OFFSET $offset;";
-      $res = self::$_db->query($query);
-      if (!$res)
-        throw new RuntimeException(self::$_db->error);
-      
-    } catch (Exception $e) {
-      throw $e;
-      // TODO anything better?
-    }
     
-    if ($res->num_rows === 0)
-      http_response_code(204); //No content
-    else {
-      
-      $i = 1;
-      while(($row = $res->fetch_array()) and $i++ !== 11){
-        $q = new Question($row['id']);
-        
-        $q->writeOut();
-      }
-      
-      return $res;
-    }
+    if (!($owner instanceof User))
+      $owner = new User($owner);
+    
+    if ($owner->deactivated)
+      throw new Exception('This user has deactivated their account.');
+    
+    if (! $owner->profileVisibleBy($GLOBALS['user']))
+      throw new Exception("You are not allowed to view this user's questions", 403);
+    
+    $query = "SELECT * FROM questions WHERE answer IS NOT NULL AND touser".
+      " = '$owner->username' ORDER BY timeanswered DESC LIMIT 11 OFFSET $offset;";
+    
+    $qset = new QuestionSet($query);
+    
+    $count = count($qset->members);
+    
+    return $qset->members; // array of \model\Questions
   }
 }
 ?>
