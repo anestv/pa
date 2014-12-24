26feff4a524a2544d08641cb887245281e35cab3
anestv <anestv@users.noreply.github.com>
2014-12-24 15:43:15 +0200

    Include SUI v1 in pages

--- app/templates/default/footer.php
+++ app/templates/default/footer.php
@@ -1,10 +1,8 @@
 <?php
 if ($data['scripts']['jquery'])
   echo '<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>';
-if ($data['scripts']['jquery.address'])
-  echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.address/1.6/jquery.address.min.js"></script>';
 if ($data['scripts']['semantic'])
-  echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/0.19.3/javascript/semantic.min.js"></script>';
+  '<script src="https://preview.c9.io/anestv/pa/dev-semantic/dist/semantic.min.js"></script>'; //TODO replace with url of the build
 if ($data['scripts']['jquery.age'])
   echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.age/1.2.4/jquery.age.min.js"></script>';
 
--- app/templates/default/header.php
+++ app/templates/default/header.php
@@ -5,12 +5,15 @@ http://github.com/anestv/pa - Open source: Artistic License 2.0 -->
 <head>
   <meta charset="utf-8">
   <title><?php if (isset($data['title']))echo $data['title'].' - '; echo SITETITLE; //SITETITLE defined in config.php?></title>
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/0.19.3/css/semantic.min.css">
-  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Comfortaa:700|Damion">
+  
   <?php
   echo '<base href="'. BASE_DIR .'">';
   if (empty($data['noGeneralCss']))
     echo '<link href="'.helpers\url::get_template_path().'css/general.css" rel="stylesheet">';
+  
+  //TODO replace with our build
+  echo '<link rel="stylesheet" href="https://preview.c9.io/anestv/pa/dev-semantic/dist/semantic.min.css">';
+  echo '<link rel="stylesheet" type="text/css" href="'.helpers\url::get_template_path()."css/topBar.css\">";
   
   if (isset($data['styles']) and is_array($data['styles']))
     foreach($data['styles'] as $style)
