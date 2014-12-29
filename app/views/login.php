8f83b4bc987ce1929212890ecfb0ca4274e58d75
anestv <anestv@users.noreply.github.com>
2014-12-29 14:32:03 +0200

    Updated login, register to SUI v1

--- app/views/login.php
+++ app/views/login.php
@@ -24,28 +24,26 @@ if ($_SESSION['userNotFound']){
 ?>
 
 <div class="center480">
-  <h3 class="ui orangeBg top attached header">Log in to <i>PrivateAsk</i></h3>
-  <form action="login" method="POST" class="ui attached form segment<?php if ($GLOBALS['warnMessage']) echo ' error';?>">
+  <h3 class="ui top attached header">Log in to <i>PrivateAsk</i></h3>
+  <form action="login" method="POST" class="ui attached form segment <?php if ($GLOBALS['warnMessage']) echo 'error';?>">
     <div class="ui error message">
       <?=$GLOBALS['warnMessage']?>
     </div>
-    <div class="ui left labeled field icon input">
-      <input name="user" required type="text" placeholder="Username" tabindex="1" autofocus>
+    <div class="ui left icon input field">
+      <input name="user" required type="text" placeholder="Username" autofocus>
       <i class="user icon"></i>
-      <div class="ui corner label"><i class="red asterisk icon"></i></div>
     </div>
-    <div class="ui left labeled field icon input">
-      <input name="pass" required type="password" placeholder="Password" tabindex="2">
+    <div class="ui left icon input field">
+      <input name="pass" required type="password" placeholder="Password">
       <i class="lock icon"></i>
-      <div class="ui corner label"><i class="red asterisk icon"></i></div>
     </div>
-    <button type="submit" class="ui right floated labeled icon positive button" tabindex="4">
-      <i class="sign in icon"></i>Log in
-    </button>
     <div class="ui toggle checkbox">
-      <input type="checkbox" name="keep" id="keep" tabindex="3">
+      <input type="checkbox" name="keep" id="keep">
       <label for="keep">Keep me logged in</label>
     </div>
+    <button type="submit" class="ui right floated right labeled icon positive button">
+      Log in<i class="sign in icon"></i>
+    </button>
   </form>
   <div class="ui attached segment">
     <a href="<?=$data['fbLoginUrl']?>" class="ui facebook fluid button">
@@ -53,9 +51,12 @@ if ($_SESSION['userNotFound']){
     </a>
   </div>
   <div class="ui bottom attached icon message">
-    <i class="signup icon"></i><div class="content">
+    <i class="signup icon"></i>
+    <div class="content">
       <div class="header">New to <i>PrivateAsk</i>?</div>
       <a href="register">Register Here</a>
-  </div></div>
+    </div>
+  </div>
 </div>
+
 </main>
