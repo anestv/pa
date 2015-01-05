17c773284e674462e3da59f1f21879a58e227cd3
anestv <anestv@users.noreply.github.com>
2015-01-05 16:13:18 +0200

    Updated registerFb and added captcha to it

--- app/views/preregisterFb.php
+++ app/views/preregisterFb.php
@@ -7,9 +7,9 @@
   
   <h2 class="ui purple center aligned block header">Do you already have a PrivateAsk account?</h2>
   <div class="ui huge centered buttons">
-    <a href="login" class="ui green button">Sure!</a>
+    <a href="login" class="ui positive button">Sure!</a>
     <span class="or"></span>
-    <a href="register/fb" class="ui blue button">Not yet</a>
+    <a href="register/fb" class="ui secondary button">Not yet</a>
   </div>
   
 </div>
